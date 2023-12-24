38872c9249abe8ff0df81299d687c3e23cf9487b
anFapp1 <putrapradana879@gmail.com>
2023-12-25 04:22:51 +0700

    meet 9

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,3 +1,4 @@
 class GlobalVariables {
-  static const baseUrl = 'https://flutterdev.my.id';
+  static const baseUrl =
+      'https://81e0-2404-8000-1028-10fb-c0e5-b9ad-45c0-6ac4.ngrok-free.app';
 }
