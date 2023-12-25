af074acc5e7ed7e062a9c62c409f939aa1d5f953
anFapp1 <putrapradana879@gmail.com>
2023-12-25 19:17:08 +0700

    meet 12

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,4 +1,4 @@
 class GlobalVariables {
   static const baseUrl =
-      'https://bdc3-2404-8000-1028-631-d1b8-845a-d5d1-7c35.ngrok-free.app';
+      'https://7961-2404-8000-1028-631-d1b8-845a-d5d1-7c35.ngrok-free.app';
 }
